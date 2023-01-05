59597f9f9ab98d83ad2c853063072c8e8456b7cd
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-04 21:38:50 -0300

    Adiciona secao params e variáveis globais com GET

---
+++ Semana_1/secao_params-e-variaveis-globais/tastecamp-back/src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors"
+
+const server = express()
+
+server.use(cors())
+
+const receitas = [
+  {
+    id: 1,
+    titulo: "Pão com Ovo",
+    ingredientes: "Ovo e pão",
+    preparo: "Frite o ovo e coloque no pão"
+  },
+  {
+    id: 2,
+    titulo: "Mingau de Whey",
+    ingredientes: "Leite, Aveia e Whey",
+    preparo: "Mistura tudo na panela fervendo",
+  },
+]
+
+server.get("/receitas", (req, responda) => {
+  responda.send(receitas)
+})
+
+server.get("/receitas/:id", (req, res) => {
+  const { id } = req.params
+
+  const receita = receitas.find((item) => item.id === Number(id))
+
+  if (receita) {
+    return res.send(receita)
+  }
+
+  res.send('Não achei a receita')
+
+})
+
+server.listen(5001, () => {
+  console.log('Servidor funfou de boas!!!')
+})
